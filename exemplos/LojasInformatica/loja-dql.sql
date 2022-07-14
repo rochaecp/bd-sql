efefa8d831b7dfa98218a308fa904494db365931
Maurício Rocha <79682437+rochaecp@users.noreply.github.com>
2022-07-14 17:56:13 -0300

    Add files via upload

---
+++ exemplos/LojasInformatica/loja-dql.sql
@@ -0,0 +1,64 @@
+USE LojasInformatica;
+
+-- =============================
+-- Consulta producao.categorias
+-- =============================
+
+SELECT *
+FROM producao.categorias;
+
+-- =============================
+-- Consulta producao.marcas
+-- =============================
+
+SELECT *
+FROM producao.marcas;
+
+-- =============================
+-- Consulta producao.produtos
+-- =============================
+
+SELECT *
+FROM producao.produtos;
+
+-- =============================
+-- Consulta vendas.clientes
+-- =============================
+
+SELECT *
+FROM vendas.clientes
+
+-- =============================
+-- Consulta vendas.lojas
+-- =============================
+
+SELECT *
+FROM vendas.lojas
+
+-- =============================
+-- Consulta vendas.funcionarios
+-- =============================
+
+SELECT *
+FROM vendas.funcionarios
+
+-- =============================
+-- Consulta vendas.pedidos
+-- =============================
+
+SELECT *
+FROM vendas.pedidos
+
+-- =============================
+-- Consulta producao.estoques
+-- =============================
+
+SELECT *
+FROM producao.estoques
+
+-- =============================
+-- Consulta vendas.itens_pedido
+-- =============================
+
+SELECT *
+FROM vendas.itens_pedido
