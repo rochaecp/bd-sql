717a30f7f325c180657942592bffc34ed5a5b512
Maurício Rocha <79682437+rochaecp@users.noreply.github.com>
2022-07-15 17:22:22 -0300

    Update loja-dql.sql

--- exemplos/LojasInformatica/loja-dql.sql
+++ exemplos/LojasInformatica/loja-dql.sql
@@ -21,6 +21,29 @@ FROM producao.marcas;
 SELECT *
 FROM producao.produtos;
 
+-- inner join (categorias)
+SELECT 
+	id_produto AS IDs, 
+	nome_produto AS Produtos, 
+	nome_categoria AS Categorias
+FROM 
+	producao.produtos p
+INNER JOIN producao.categorias c
+	ON c.id_categoria = p.id_categoria 
+
+-- inner join (categorias e marcas)
+SELECT 
+	id_produto AS IDs,
+	nome_produto AS Produtos,
+	nome_categoria AS Categorias,
+	nome_marca AS Marcas
+FROM 
+	producao.produtos p
+INNER JOIN producao.marcas m
+	ON m.id_marca = p.id_marca
+INNER JOIN producao.categorias c
+	ON c.id_categoria = p.id_categoria
+
 -- =============================
 -- Consulta vendas.clientes
 -- =============================
